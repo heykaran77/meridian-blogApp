87e2d97e9312042510409a07708d58036d5325cc
Karan Singh <devkaran.codes@gmail.com>
2026-01-03 20:19:49 +0530

    feat: add Convex client provider with better auth.

--- components/providers/ConvexClientProvider.tsx
+++ components/providers/ConvexClientProvider.tsx
@@ -5,9 +5,7 @@ import { ConvexReactClient } from "convex/react";
 import { authClient } from "@/lib/auth-client";
 import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
-  expectAuth: true,
-});
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({
   children,
